8992bc095e98abf025f957c19135b8369c619ef5
Felix <felix_1999@live.no>
2022-11-08 18:38:46 +0100

    :tada: Setup the project

---
+++ src/components/BotComList/BotComList.jsx
@@ -0,0 +1,86 @@
+/* import '../style/botComList.css'; */
+import BotCompany from '../BotCompany';
+import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const StyledBotComList = styled.div` 
+ul {
+
+}
+`
+
+
+
+function BotComList() {
+	  const [names, setNames] = useState(null);
+	  useEffect(() => {
+		const getNames = async() => {
+			const {data} = await axios.get('http://localhost:5000/names/10')
+			setNames(data);
+		}
+		if (names) return;
+		getNames();
+		
+
+	  }, []);
+  return (
+   <StyledBotComList>
+	<ul>
+		{names && names.map((name, idx) => {
+			return <BotCompany name={name} key={idx} description="null" />
+		})
+		} 
+	</ul>
+	</StyledBotComList>
+  );
+}
+
+const botCompanies = [
+	{ name: "dfsdf", description: 'This is company 1' },
+	{ name: "sdfsdf", description: 'This is company 2' },
+	{ name: "sdfsdf", description: 'This is company 3' },
+	{ name: "dsfsdf", description: 'This is company 4' },
+	{ name: "sdfsdf", description: 'This is company 5' },
+	{ name: "dsf", description: 'This is company 6' },
+];
+
+/* const readTextFile = (file) => {
+	const textContent = readFileSync(file, 'utf-8');
+	
+
+	const arr = textContent.split(/\r?\n/);
+	return arr;
+} */
+
+/* const generateRandIndustry = () => {
+	const txtFileDir = './industries.txt';
+	const industries = readTextFile(txtFileDir);
+	console.log(industries, 'industries');
+	const randIndustry = industries[Math.floor(Math.random()*industries.length)];
+	return randIndustry;
+} */
+
+/* const generateRandName = () => {
+	const txtFileDir = './names.txt';
+	const names = readTextFile(txtFileDir);
+	const randName = names[Math.floor(Math.random()*names.length)];
+	return randName;
+} */
+
+/* const generateRandSuffix = () => {
+	const txtFileDir = './business-Suffixes.txt';
+	const suffix = readTextFile(txtFileDir);
+	const randSuffix = suffix[Math.floor(Math.random()*suffix.length)];
+	return randSuffix;
+} */
+
+/* const generateCompanyName = () => {
+	const industry = generateRandIndustry();
+	const name = generateRandName();
+	const suffix = generateRandSuffix();
+	const companyName = `${name}'s ${industry} ${suffix}`;
+	console.log(companyName);
+} */
+
+export default BotComList;
---
+++ src/components/BotCompany.jsx
@@ -0,0 +1,14 @@
+/* import '../style/botCompany.css'; */
+
+function BotCompany(companyInfo) {
+  return (
+   <li>
+		<h3>{companyInfo.name}</h3>
+		<p>{companyInfo.description}</p>
+	</li>
+  );
+}
+
+
+
+export default BotCompany;
