073d4787c266df81323cf3ed47e24a201940e865
MZSkjaerstad <martskjae@hotmail.no>
2022-11-20 00:12:36 +0100

    :sparkles: Create firebase authentication

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-{/*           <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <userAuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+  {/*           <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </userAuthContextProvider>
   );
 }
 
