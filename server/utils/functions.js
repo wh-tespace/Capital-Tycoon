f142383666c867f245d9c2e80cf17a1986a94764
Felix <felix_1999@live.no>
2022-11-20 03:28:45 +0100

    :sparkles: Generate companies

--- server/utils/functions.js
+++ server/utils/functions.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const FirebaseBotCompany = require("../config/firebaseConfig")
 const app = express()
-app.use(express.json())
-app.use(cors())
 
 const personNames = require('../jsonFiles/names.json');
 const industires = require('../jsonFiles/industries.json');
 const suffix = require('../jsonFiles/business-Suffixes.json');
 
-const storeBotCompany = (botCompany) => {
-	app.post('/create/', async (req, res) => {
-		const data = req.body;
-		console.log("Data of users", data)
-		await FirebaseBotCompany.add(data);
-		res.send({ message: 'success botCompany added' });
-	});
-}
+const firebaseFunctions = require("./firebaseFunctions")
 
 const generateRandName = () => {
 	const randName = (personNames.names[Math.floor(Math.random()*personNames.names.length)]);
@@ -39,18 +29,88 @@ const generateCompanyName = () => {
 	const suffix = generateRandSuffix();
 
 	const companyName = `${name}'s ${industry} ${suffix}`;
-	console.log(companyName, "nameeee")
+	console.log(companyName, "name generated")
 	return companyName
+}
+
+const generateNewDate = () => {
+	const newDate = new Date().getTime()
+	return newDate
+}
+
+const generateId = () => {
+	const newDate = generateNewDate()
+	const rndNum = Math.floor(Math.random() * 100000);
+	const id = `${newDate}${rndNum}`
+	return id
+}
+
+const genRandNum = (min, max) => {
+	return Math.floor(Math.random() * (max - min +1) + min)
+}
+
+const generateShares = () => {
+	const availableShares = 10_000_000
+	const sharesOnMarket = genRandNum(1000, availableShares);
+	console.log(sharesOnMarket, "sharesonmarket")
+	return sharesOnMarket
+}
+
+const generateStockPrice = () => {
+	const stockPrice = genRandNum(0.1, 1000);
+	return stockPrice
+}
+
+const generateRandomColor = () => {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	return randomColor
+}
+
+const generateIdenticon = (companyId) => {
+	const imageApiIdx = genRandNum(0, 3)
+	const imageId = companyId
+	const imageSize = 128
+	let imageUrl = ``
+	switch(imageApiIdx) {
+		case 0:
+			imageUrl = `https://source.boringavatars.com/bauhaus/${imageSize}/${imageId}?colors=264653,2a9d8f,e9c46b`
+		  break;
+		case 1:
+			imageUrl = `https://source.boringavatars.com/beam/${imageSize}/${imageId}?colors=${generateRandomColor()},${generateRandomColor()},${generateRandomColor()}`
+		  break;
+		case 2:
+			imageUrl = `source.boringavatars.com/pixel/${imageSize}/${imageId}?colors=${generateRandomColor()},${generateRandomColor()},${generateRandomColor()}`
+			break;
+		default:
+			imageUrl = `https://identicon-api.herokuapp.com/${imageId}/${imageSize}?format=(svg)`
+	 }
+	
+	return imageUrl
 }
 
 const generateNewCompany = () => {
 	const newCompany = [];
 	const newCompanyName = generateCompanyName();
+	const dateFounded = generateNewDate()
+	const id = generateId()
+	const stockPrice = generateStockPrice()
+	const availableShares = generateShares()
+	const netValue = stockPrice * availableShares
+	const companyLogo = generateIdenticon(id)
 
-	newCompany.push({ cName: newCompanyName });
-	storeBotCompany(newCompany);
+	newCompany.push({
+		id: id,
+		companyName: newCompanyName,
+		dateFounded: dateFounded,
+		availableShares: availableShares,
+		stockPrice: stockPrice,
+		netValue: netValue,
+		companyLogo: companyLogo
+	});
+	console.log("sending to firebaseFunction", newCompany)
+	firebaseFunctions.storeBotCompany(newCompany);
 }
 
 generateNewCompany();
-
+firebaseFunctions.deleteBotCompany("*")
 module.exports = { generateNewCompany };
