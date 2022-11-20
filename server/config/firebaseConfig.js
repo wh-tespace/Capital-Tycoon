bc333d44ff220ec2e5f29a11db190562f76d6de9
Felix <felix_1999@live.no>
2022-11-20 03:26:41 +0100

    :wrench: Add config file for firebase

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,17 +1,12 @@
-const firebase = require('firebase');
-const firebaseConfig = {
-	apiKey: "AIzaSyAkk0YArj4SoR9nKfKy4cqw8EDwbBA43CU",
-	authDomain: "reactproject-4a1d6.firebaseapp.com",
-	projectId: "reactproject-4a1d6",
-	storageBucket: "reactproject-4a1d6.appspot.com",
-	messagingSenderId: "1067526123016",
-	appId: "1:1067526123016:web:e8b338ea56ea3d4934abbd"
-};
+const { initializeApp, cert } = require("firebase-admin/app")
+const { getFirestore } = require("firebase-admin/firestore")
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const botCompanies = db.collection("botCompanies")
+const serviceAccount = require("./creds.json")
 
-/* const botCompany = db.collection('botCompany'); */
+initializeApp( {
+	credential: cert(serviceAccount)
+})
 
-module.exports = botCompanies;
+const db = getFirestore()
+
+module.exports = { db }
